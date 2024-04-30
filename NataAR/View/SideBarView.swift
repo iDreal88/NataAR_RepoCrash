052f20badb4228ad71019eb8f2473300d9b08d33
Oey Darryl Valencio Wijaya <70695383+iDreal88@users.noreply.github.com>
2024-04-30 22:52:52 +0700

    update code debug

---
+++ NataAR/View/SideBarView.swift
@@ -0,0 +1,18 @@
+//
+//  SideBarView.swift
+//  NataAR
+//
+//  Created by Denny Chandra Wijaya on 28/04/24.
+//
+
+import SwiftUI
+
+struct SideBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SideBarView()
+}
